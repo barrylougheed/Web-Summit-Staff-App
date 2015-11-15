558c04c49c8a081fb0f08616615199fa06945ab0
Hassan <h.alamidi@yahoo.co.uk>
2015-11-15 11:29:55 +0000

    fade in effect while figure out perfect slide effect

--- JS/frameBehaviour.js
+++ JS/frameBehaviour.js
@@ -1,10 +1,34 @@
 
+//$(document).ready(function(){
+//$('#home, #test').click(function(){
+//	if(this.id == 'home'){
+//		$("#homemain").load("./test.txt");
+//	}else if(this.id == 'test'){
+//		$("#homemain").load("./hassantest.txt");
+//	}
+//});
+//});
+
 $(document).ready(function(){
-$('#home, #test').click(function(){
-	if(this.id == 'home'){
-		$("#homemain").load("./test.txt");
-	}else if(this.id == 'test'){
-		$("#homemain").load("./hassantest.txt");
-	}
-});
+	$('#home, #test').click(function(){
+		if(this.id == 'test'){
+		  				
+			$("#homemain").fadeOut(1000,"swing",function(){
+				//$("#homemain2").removeClass("inactive").addClass("active");
+				$("#homemain2").load("./hassantest.txt");
+				$("#homemain2").fadeIn(1000,"linear");
+				//$("#homemain").removeClass("active").addClass("inactive");
+			});
+			$("#homemain2").fadeIn("slow");
+		}else if(this.id == 'home'){
+			$("#homemain2").fadeOut(1000,"swing",function(){
+				//$("#homemain").removeClass("inactive").addClass("active");
+				$("#homemain").load("./test.txt");
+				$("#homemain").fadeIn(1000,"linear");
+				//$("#homemain2").removeClass("active").addClass("inactive");
+			});
+			//$("#homemain").fadeIn("slow");
+		}
+
+	});
 });
