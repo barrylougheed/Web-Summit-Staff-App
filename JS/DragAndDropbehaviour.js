e94a48055979c33b97d9ac1d51b092bdb3d67b19
stephenkearns1 <Sean@Sean-Murrays-MacBook-Pro.local>
2015-11-30 22:47:34 +0000

    Linus why dont youu count my commits

--- JS/DragAndDropbehaviour.js
+++ JS/DragAndDropbehaviour.js
@@ -13,9 +13,9 @@
                 
                 containment:"document",
                 zIndex: 2000,
-                revert: "invaild",
+                revert: true,
                 drag:function(event, ui){
-                    $('.draggable').addClass("tempStyleDraggable");
+                   $(this).addClass("tempStyleDraggable");
                 }
 
              
@@ -26,13 +26,18 @@
              $('#dropable').droppable({
                  accept:".draggable",
                  drop:function(event,ui){
-                    console.log("Working bitches");
-                    $('.draggable').removeClass("tempStyleDraggable");
+                    console.log("Working");
+                    $(this).removeClass("tempStyleDraggable");
+                    $('.draggable').addClass("talkholder");
+
+
                 }
                 //create a function that is actived on, the drop of element
                 // when the element is dropped grab the info from within and use ajax to display it 
 
              });
+
+             console.log("My gits are not counting thank you linus")
 
            /*  var pos = draggable.getBoundingClientRect();
              console.log(pos.top, pos.right, pos.bottom)  */
