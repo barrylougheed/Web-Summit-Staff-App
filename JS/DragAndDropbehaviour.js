f016aad99c8448e8f23f52009119e30c2c709f5e
stephenkearns1 <Sean@Sean-Murrays-MacBook-Pro.local>
2015-11-30 15:15:01 +0000

    Version 54

--- JS/DragAndDropbehaviour.js
+++ JS/DragAndDropbehaviour.js
@@ -1,12 +1,37 @@
  $(document).ready(function(){     
      $(function(){
-             $('.draggable').draggable({
-                revert: true
-                //zIndex: 1
-             });
 
-             $('.dropable').droppable({
-                //transfer data on drop
+        
+          // var $timesList = ('#draggable');
+          //    $($timesList,'li').draggable({
+          //         revert: true
+
+          //      });
+
+             var $index = ('.navbar-fixed-top');
+             $('.draggable').draggable({
+                
+                containment:"document",
+                zIndex: 2000,
+                revert: "invaild",
+                drag:function(event, ui){
+                    $('.draggable').addClass("tempStyleDraggable");
+                }
+
+             
+             }); /*function(){
+                 $($index).css("index",0);
+               });*/
+         
+             $('#dropable').droppable({
+                 accept:".draggable",
+                 drop:function(event,ui){
+                    console.log("Working bitches");
+                    $('.draggable').removeClass("tempStyleDraggable");
+                }
+                //create a function that is actived on, the drop of element
+                // when the element is dropped grab the info from within and use ajax to display it 
+
              });
 
            /*  var pos = draggable.getBoundingClientRect();
